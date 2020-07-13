53b9810edb599a86665313c9d82b769965373834
guenael <guenael@atlanticlog.org>
2020-07-13 11:14:51 +0200

    test2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs'); // file system
 
 const URL = "https://www.iban.com/structure";
 
-const DIGIT_REG = "\d";
+// const DIGIT_REG = "\d";
 const test = 20;
 let stringTest = "FR";
 console.log(`FR\\d{${test}}`);
@@ -15,39 +15,53 @@ console.log(stringTest);
 
 
 
-  class Regex {
+class Regex {
 
-    constructor(){
-      this.countryCode = "";
-      this.value = "";
+  static get DIGIT_REG() { return String.raw`\d`; }
+
+
+  constructor(){
+    this.countryCode = "";
+    this.value = "";
+    this.letterCount = 0;
+    this.digitCount = 0;
+    this.otherCount = 0;
+  }
+
+  testAddLetter() {
+    if (this.letterCount != 0) {
+      this.addLetter();
       this.letterCount = 0;
+    }
+  }
+  testAddDigit() {
+    if ( this.digitCount != 0 ) {
+      this.addDigit();
       this.digitCount = 0;
+    }
+  }
+  testAddOther() {
+    if ( this.otherCount != 0 ) {
+      this.addOther();
       this.otherCount = 0;
     }
-
-    testLetter() {
-      if (this.letterCount == 0) {
-        this.value += `[A-Z]{${letterCount}}`;
-        this.letterCount = 0;
-      }
-    }
-
-    testDigit() {
-      if ( this.digitCount == 0 ) {
-        this.value += `\\d{${digitCount}}`;
-        this.digitCount = 0;
-      }
-    }
-
-    testOther() {
-      if ( this.otherCount == 0 ) {
-        this.value += `.{${otherCount}}`;
-        this.otherCount = 0;
-      }
-    }
-
-
   }
+
+  addDigit() {
+    this.value += `\\d{${this.digitCount}}`;
+    // console.log(Regex.DIGIT_REG);
+    // this.value += Regex.DIGIT_REG + `{${this.digitCount}}`;
+    // this.value += String.raw`\d`;
+  }
+  addLetter() {
+    this.value += `[A-Z]{${this.letterCount}}`;
+  }
+  addOther(){
+    this.value += `.{${this.otherCount}}`;
+  }
+
+}
+
 
 rp(URL)
   .then(html => {
@@ -75,16 +89,16 @@ rp(URL)
           } else { // plus les premières lettres
             if (isCountryCode) { isCountryCode = false; }
             if ( char.match(/[a-z]/i) ) { // si lettre
-              regex.testDigit();
-              regex.testOther();
+              regex.testAddDigit();
+              regex.testAddOther();
               regex.letterCount++;
             } else if ( char.match(/\d/) ) { // si chiffre
-              regex.testLetter();
-              regex.testOther();
+              regex.testAddLetter();
+              regex.testAddOther();
               regex.digitCount++;
             } else { // Si autre ( à priori impossible )
-              regex.testLetter();
-              regex.testDigit();
+              regex.testAddLetter();
+              regex.testAddDigit();
               regex.otherCount++;
             }
 
@@ -92,23 +106,26 @@ rp(URL)
 
       }
       if ( regex.digitCount != 0 && regex.letterCount == 0 && regex.otherCount == 0 ) {
-        regex.value += `\\d{${digitCount}}`;
+        regex.addDigit();
         regexMap.set(regex.countryCode, regex.value);
       } else if ( regex.letterCount != 0 && regex.digitCount == 0 && regex.otherCount == 0 ){
-        regex.value += `[A-Z]{${letterCount}}`;
+        regex.addLetter();
         regexMap.set(regex.countryCode, regex.value);
       } else if ( regex.otherCount != 0 && regex.letterCount == 0 && regex.digitCount == 0 ){
-        regex.value += `.{${otherCount}}`;
+        regex.addOther();
         regexMap.set(regex.countryCode, regex.value);
       } else {
         console.error("\x1b[31m", `Erreur lors du parsing IBAN, code pays : ${regex.countryCode} - Pas mis dans le regex`, "\x1b[5m", "/!\\", "\x1b[0m");
       }
 
+
       codeLengthMap.set(regex.countryCode, iban.length - regex.countryCode.length);
 
     });
 
-    console.log(regexMap);
+    const sorted2 = new Map([...regexMap.entries()].sort());
+    console.log(sorted2);
+
     const sortedMap = new Map([...codeLengthMap.entries()].sort((a, b) => a[1] - b[1]));
     const mapIterator = sortedMap.entries();
 
